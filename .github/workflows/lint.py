8bdd1f0c1f415205dbb469756687556544d3d17b
Ventsislav Georgiev <90458160+vencigeorgiev@users.noreply.github.com>
2021-11-10 12:49:28 +0200

    Delete lint.py

--- .github/workflows/lint.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pylint import lint
-THRESHOLD = 5.7
-run = lint.Run(["factorial.py"], do_exit=False)
-score = run.linter.stats["global_note"]
-if score < THRESHOLD:
-    print("Linter failed: Score < threshold value")
-    sys.exit(1)
-sys.exit(0)
